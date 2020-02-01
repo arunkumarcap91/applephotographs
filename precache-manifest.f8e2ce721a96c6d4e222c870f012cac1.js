4fefa64dccee236fa8db0cc2219d8312c64c5e10
Arunkumar C <arunkumar.chellappandi@cesltd.com>
2020-02-02 01:57:57 +0530

    Updates

---
+++ precache-manifest.f8e2ce721a96c6d4e222c870f012cac1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15cd7223e9ae44e6025bc0d2420e0be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ac4877ad01908fb5e34",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "0ac4877ad01908fb5e34",
+    "url": "/static/js/2.55a3ac76.chunk.js"
+  },
+  {
+    "revision": "25ef6bac65b57b7c10ba8f99129dc096",
+    "url": "/static/js/2.55a3ac76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2eb3ebf8cad57b7e8e3d",
+    "url": "/static/js/main.5c30961a.chunk.js"
+  },
+  {
+    "revision": "2f36df705ac37a44d470",
+    "url": "/static/js/runtime-main.2e7fe822.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
